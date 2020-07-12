4cef01324224eb51f36b0a805a61845d5e1ad26f
Gabriel Marques <88gabrielmarques@gmail.com>
2020-07-11 23:21:15 -0300

    v1

---
+++ precache-manifest.c62348cc9bc7ff3caf88315b1e0299f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f48ebce714948fca5948",
+    "url": "/static/css/main.08a166d2.chunk.css"
+  },
+  {
+    "revision": "f48ebce714948fca5948",
+    "url": "/static/js/main.f48ebce7.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d2a8142417262f3d68d6d68be3e41a75",
+    "url": "/index.html"
+  }
+];
